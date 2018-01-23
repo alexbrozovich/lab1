4e78b0ea5e90346d02e8c0a1888c42290a248ca5
Alex Brozovich <alexbrozovich@Alexs-MacBook-Pro.local>
2018-01-22 22:43:02 -0500

    Added comments to the code like a good programmer :)

--- src/lab1.java
+++ src/lab1.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 public class lab1 {
 
 	public static void main(String[] args) {
-		/*
-		 * Prompt the user to enter values of length and width of the classroom Display
-		 * the area and perimeter of that classroom Ask if the user wants to continue
-		 * (y/n) Assume the rooms are perfect rectangles Assume the user will enter
-		 * valid numeric
-		 */
+		
 		Scanner scan = new Scanner(System.in);
+		
+		//keeps the program looping until the user says not to continue
 		boolean loop = true;
 		while (loop == true) {
+			//takes user input for classroom length and width
 			System.out.println("Enter classroom length: ");
 			String length = scan.nextLine();
 			System.out.println("Enter classroom width: ");
 			String width = scan.nextLine();
+			
+			//converts user input to double for easy addition / multiplication
 			Double doubleLength = Double.valueOf(length);
 			Double doubleWidth = Double.valueOf(width);
+			
+			//calculate and print requested data
 			System.out.println("Area of the classroom is " + (doubleLength * doubleWidth));
 			System.out.println("Perimeter of the classroom is " + ((doubleLength + doubleWidth) * 2));
+			
+			//check to make sure the user input is good. if it isn't y or n, this section will keep repeating until it gets a real answer
 			Boolean goodInput = false;
 			while (goodInput == false) {
 				System.out.println("Would you like to continue? (y/n)");
